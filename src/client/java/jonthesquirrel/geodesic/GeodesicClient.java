670ea948edd95e54653287d32cf91a89234ed29a
jonthesquirrel <jdh11235@gmail.com>
2024-08-01 10:57:16 -0500

    Generate Fabric mod template

---
+++ src/client/java/jonthesquirrel/geodesic/GeodesicClient.java
@@ -0,0 +1,10 @@
+package jonthesquirrel.geodesic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GeodesicClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
